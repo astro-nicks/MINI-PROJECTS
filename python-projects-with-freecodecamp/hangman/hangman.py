cd9dfb8a2de0c43aedc88559658957108cd3b6fe
astro-nicks <astronix05@gmail.com>
2025-03-13 19:41:19 +0000

    work in progress

--- python-projects-with-freecodecamp/hangman/hangman.py
+++ python-projects-with-freecodecamp/hangman/hangman.py
@@ -1,9 +1,20 @@
 import random
 from word import words 
+import string
 
 def get_valid_word(words):
     word = random.choice(words)  #randomly chooses something from the list
-    while '-' in word '' in word:
+    while '-' in word or '' in word:
         word = random.choice(words)
 
     return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+
+user_input = input("Type Something:")
+print(user_input)
