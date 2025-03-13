344100bab945572f10a65b1b3d0833738dda779c
astro-nicks <astronix05@gmail.com>
2025-03-13 20:11:49 +0000

    few more additions

--- python-projects-with-freecodecamp/hangman/hangman.py
+++ python-projects-with-freecodecamp/hangman/hangman.py
@@ -15,6 +15,32 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print("You have used these letters: ", ''.join(used_letters))
 
-user_input = input("Type Something:")
-print(user_input)
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ''.join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again.")
+
+        else:
+            print("Invalid character. Please try again.")
+
+    # gets here when len(word_letters) == 0
+
+hangman()
+
+
+#user_input = input("Type Something:")
+#print(user_input)
