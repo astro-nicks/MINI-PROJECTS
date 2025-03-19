bdcfecc144c2e46b88a88ebd2c0426f0e1655962
astro-nicks <astronix05@gmail.com>
2025-03-19 10:42:57 +0000

    Percentiles

--- ML Fundamentals/Percentiles.py
+++ ML Fundamentals/Percentiles.py
@@ -1,8 +1,14 @@
 import numpy as np
+import matplpotlib.pyplot as plt
 
 
-data = [12,14,118,20,16]
-p_25 = np.percentiles(data, q:25)
-p_50 = np.percentiles(data, q:50)
-p_75 = np.percentiles(data, q:75)
+data = [12,14,18,20,16]
+p_25 = np.percentile(data, 25)
+p_50 = np.percentile(data, 50)
+p_75 = np.percentile(data, 75)
+
+print(f"25th Percentile: {p_25}")
+print(f"50th Percentile (Median): {p_50}")
+print(f"75th Percentile: {p_75}")
+
 
